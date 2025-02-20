e056d8170a52388dd2d7c0021908db4f7e7b22f5
nikolay-kulinkin <kulinkin.nik@gmail.com>
2025-02-20 22:53:44 +0300

    Lesson 10

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,13 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\View;
 
 class HomeController extends Controller
 {
   public function index()
   {
-    return view('home.index',['title'=>'Home page','test'=>'Test']);
+    $users=json_decode(file_get_contents('https://jsonplaceholder.typicode.com/users'),true);
+    // $users=Http::get('https://jsonplaceholder.typicode.com/users')->json();
+    // dump($users);
+    return view('home.index',compact('users'));
   }
 
   public function test()
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -36,7 +36,7 @@
        @yield('content')
     </div>
 
-    @include('layouts.incs.footer',['test'=>'Hello from layout'])
+    <!-- @include('layouts.incs.footer',['test'=>'Hello from layout']) -->
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js">
     </script>
